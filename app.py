e8b71a0c89493fd601e22b8268e2eb315fe83004
Jonny Grotenhuis <grotenhuis@google.com>
2020-07-26 18:50:46 -0700

    Challenge Code for Testing

--- app.py
+++ app.py
@@ -13,12 +13,12 @@ def index():
     mars = mongo.db.mars.find_one()
     return render_template("index.html", mars=mars)
 
-app.route("/scrape")
+@app.route("/scrape")
 def scrape():
     mars = mongo.db.mars
     mars_data = scraping.scrape_all()
     mars.update({}, mars_data, upsert=True)
-    return "Scraping Successful!"
+    return "Scraping and Four Hemisphere Photos Successful!"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
